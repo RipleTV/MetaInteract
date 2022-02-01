ab4826ad5385b0f6a8b4c23a8b400b9501d3e93d
RipleTV <gryshyn2000@gmail.com>
2022-02-01 13:34:27 +0200

    v1.1 Cleared, comented, optimised.

--- editNumber.js
+++ editNumber.js
@@ -1,45 +1,32 @@
 import numberArtifact from "../artifacts/contracts/dapp.sol/Counter.json" assert { type: "json" }
+const numberAddr = '0x8Faa19f840AD214A4804606d06a396B2114F51D3'
 
-document.querySelector(".increaseButton").onclick = async function() {
-        if (window.ethereum) {
-            const provider = new ethers.providers.Web3Provider(window.ethereum, "any");
-            await provider.send("eth_requestAccounts", []);
-            const signer = provider.getSigner();
+    // connect to eth by provider (metamask)
+if (window.ethereum) {
+    const provider = new ethers.providers.Web3Provider(window.ethereum, "any");
+    await provider.send("eth_requestAccounts", []);
+    const signer = provider.getSigner();
+    console.log("Account:", await signer.getAddress());
 
-            const numberAddr = '0x8Faa19f840AD214A4804606d06a396B2114F51D3'
-            
-            const numberContract = new ethers.Contract(
-                numberAddr,
-                numberArtifact.abi,
-                signer
-            )
+        // implement our contract from blockchain
+    const numberContract = new ethers.Contract(
+        numberAddr,             // contract address
+        numberArtifact.abi,     // contract Abi from file .../Counter.json
+        signer                  // user's address from provider
+    )
+        // update data from contract
+    document.querySelector('.headText').innerHTML = await numberContract.value()        
 
-            await numberContract.increese()
-                
-            document.querySelector('.headText').innerHTML = await await numberContract.value()
-        } else {
-            alert('You must install MetaMask')
-        }
-}
-
-document.querySelector(".decreaseButton").onclick = async function() {
-    if (window.ethereum) {
-        const provider = new ethers.providers.Web3Provider(window.ethereum, "any");
-        await provider.send("eth_requestAccounts", []);
-        const signer = provider.getSigner();
-
-        const numberAddr = '0x8Faa19f840AD214A4804606d06a396B2114F51D3'
-
-        const numberContract = new ethers.Contract(
-            numberAddr,
-            numberArtifact.abi,
-            signer
-        )
-
-        await numberContract.degreese()
-            
-        document.querySelector('.headText').innerHTML = await await numberContract.value()
-    } else {
-        alert('You must install MetaMask')
+        // button increase 
+    document.querySelector(".increaseButton").onclick = async function() {
+        await numberContract.increese() // use function increase from contract
+        document.querySelector('.headText').innerHTML = await numberContract.value() // show value of number 
     }
+        // button decrease 
+    document.querySelector(".decreaseButton").onclick = async function() {
+        await numberContract.degreese() // use function decrease from contract
+        document.querySelector('.headText').innerHTML = await numberContract.value() // show value of number 
+    }
+} else {
+    alert('Something went wrong! Please restart page and try again.')
 }
